3b6e7c81f08bc1474e81581988c6d4a9a05c338f
0xjesus <pdlgmcn@protonmail.com>
2025-09-23 00:28:57 -0600

    ci: add regenerate workflows; tests: add coverage; docs: usage + badges

---
+++ Packages/NearJsonRpcClient/Tests/NearRPCClientTests/ForceSlashTransportTests.swift
@@ -0,0 +1,15 @@
+import XCTest
+@testable import NearJsonRpcClient
+import NearJsonRpcTypes
+
+final class ForceSlashTransportTests: XCTestCase {
+    func testTransportForcesSlashPath() throws {
+        let t = ForceSlashTransport(baseURL: URL(string: "https://rpc.mainnet.near.org")!)
+        var comps = URLComponents()
+        comps.scheme = "https"
+        comps.host = "rpc.mainnet.near.org"
+        comps.path = "/should/be/ignored"
+        let (url, _) = try t.makeURLRequest(path: comps.path, body: Data("{}".utf8), headers: [:]) // helper interno si existe
+        XCTAssertEqual(url.path, "/") // path forzado a "/"
+    }
+}
